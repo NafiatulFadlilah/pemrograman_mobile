ac033df5d025a6249920ed8915512705a7b0bfa4
Nafiatul Fadlilah <nfdnafiatulfa@gmail.com>
2023-09-26 12:34:18 +0700

    re-init pertemuan 5

---
+++ pertemuan5/navigasi_rute/navigasi_rute/lib/models/item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  String name;
+  int price;
+  int stock;
+  String images;
+
+  Item(
+      {required this.name,
+      required this.price,
+      required this.stock,
+      required this.images});
+}
